34ba08bf493f81970a2e2bd233eedb99a3e6ca95
mfurqankhan02-ai <mfurqankhan02@gmail.com>
2026-03-31 12:38:57 +0500

    Test Code1
    Sum of two arrays using numpy

---
+++ testing.py
@@ -0,0 +1,12 @@
+#import Numpy library
+import numpy as np
+
+#Introducing arr1 and arr2
+arr1 = np.array([1,2,3])
+arr2 = np.array([2,3,4])
+
+#Summation of arr1 and arr2 
+arr3 = arr1+arr2
+
+#printing the result
+print(arr3)
